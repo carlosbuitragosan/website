62ca793206760f99528f6965b0164badddf50a42
Carlos Buitrago <letscatchupagain@gmail.com>
2023-02-27 23:40:25 +0000

    build - added audio files and buttons. can make a single button work but not all together. Future fix

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hi')
+// const audioMbira = document.querySelector('.audio__mbira');
+const playerButtons = document.querySelectorAll('.player__button');
+// const buttonMbira = document.querySelector('.button__mbira');
+
+let count = 0;
+const playPause = () => {
+    if (count === 0) {
+        count = 1;
+        audio.play();
+        // buttonMbira.innerHTML = '| | THUMB PIANO - ZIMBABWE';
+    } 
+    else {
+        count = 0;
+        audio.pause();
+        // buttonMbira.innerHTML = '&#9658 THUMB PIANO -ZIMBABWE';
+    }
+};
+
+playerButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(button.classList.value)
+        playPause();
+    });
+})
