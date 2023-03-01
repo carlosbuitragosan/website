d766ee98eae5e428b70faa6667c4515aca9a5732
Carlos Buitrago <letscatchupagain@gmail.com>
2023-03-01 09:50:41 +0000

    style - adjusted buttons and styled background img

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-// const audioMbira = document.querySelector('.audio__mbira');
-const playerButtons = document.querySelectorAll('.player__button');
+// const audioMbira = document.querySelector('.button__mbira + audio');
+// const playerButtons = document.querySelectorAll('.player__button');
 // const buttonMbira = document.querySelector('.button__mbira');
 
-let count = 0;
-const playPause = () => {
-    if (count === 0) {
-        count = 1;
-        audio.play();
-        // buttonMbira.innerHTML = '| | THUMB PIANO - ZIMBABWE';
-    } 
-    else {
-        count = 0;
-        audio.pause();
-        // buttonMbira.innerHTML = '&#9658 THUMB PIANO -ZIMBABWE';
-    }
-};
 
-playerButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(button.classList.value)
-        playPause();
-    });
-})
+// let count = 0;
+// const playPause = () => {
+//     if (count === 0) {
+//         count = 1;
+//         audioMbira.play();
+//         buttonMbira.innerHTML = '| | &nbsp THUMB PIANO';
+//     } 
+//     else {
+//         count = 0;
+//         audioMbira.pause();
+//         buttonMbira.innerHTML = '&#9658 &nbsp THUMB PIANO';
+//     }
+// };
+
+ 
+//     buttonMbira.addEventListener('click', event => {
+//         let clickedValue = [`${event.currentTarget.classList.value}`.replace(' ', ', ')];
+//         console.log(clickedValue)
+
+//         // let clickedAudio = document.querySelector(`${clickedValue} + audio`)
+//         // console.log(clickedAudio)
+        
+//     });
+
+//     /////////////////////////////////////////////////////////////////////////
+
+const audioData = [
+    ['mbira', {
+        url : 'audio/Mbira.mp3',
+        name: 'thumb piano'
+    }],
+    ['cretan', {
+        url: 'audio/Sa-dakri.mp3',
+        name: 'cretan song'
+    }],
+    ['guitar', {
+        url: 'audio/guitar.mp3',
+        name: 'guitar impro'
+    }],
+    ['chant', {
+        url: 'audio/chant.mp3',
+        name: 'indian chant'
+    }]
+];
+console.log(audioData[0])
