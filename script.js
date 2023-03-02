546167dfcfdbb24e0d75f0737a810541a36cbc27
Carlos Buitrago <letscatchupagain@gmail.com>
2023-03-02 10:57:05 +0000

    refactor - started player again with audio web API. testing CORS

--- script.js
+++ script.js
@@ -29,72 +29,111 @@
 
 //     /////////////////////////////////////////////////////////////////////////
 
-const audioData = {
-    'mbira': {
-        url : 'audio/Mbira.mp3',
-        name: 'thumb piano'
-    },
-    'cretan': {
-        url: 'audio/Sa-dakri.mp3',
-        name: 'cretan song'
-    },
-    'guitar': {
-        url: 'audio/guitar.mp3',
-        name: 'guitar impro'
-    },
-    'chant': {
-        url: 'audio/chant.mp3',
-        name: 'indian chant'
+// const audioData = {
+//     'mbira': {
+//         url : 'audio/Mbira.mp3',
+//         name: 'thumb piano'
+//     },
+//     'cretan': {
+//         url: 'audio/Sa-dakri.mp3',
+//         name: 'cretan song'
+//     },
+//     'guitar': {
+//         url: 'audio/guitar.mp3',
+//         name: 'guitar impro'
+//     },
+//     'chant': {
+//         url: 'audio/chant.mp3',
+//         name: 'indian chant'
+//     }
+// };
+
+
+// const musicPlayer = document.querySelector('.music__player');
+// const audioDataArray = Object.entries(audioData);
+
+
+// audioDataArray.forEach(track => {
+//     musicPlayer.insertAdjacentHTML('beforeend', 
+
+//     `<div class="player__audio">
+
+//         <button class="${track[0]}">&#9658 &nbsp ${track[1].name.toUpperCase()} </button>
+
+//         <audio id="${track[0]}" class="audio audio__${track[0]} controls">
+//             <source src="${track[1].url}" type="audio/mp3">
+//         </audio>
+//     </div>`)
+// });
+
+
+// const allAudio = document.querySelectorAll('#mbira, #guitar, #chant, #cretan');
+// let count = 0;
+
+// const playPause = (song, object) => {
+    
+//     let track = document.querySelector(`#${song}`);
+//     let button = document.querySelector(`.${song}`);
+
+//     if (count === 0) {
+//         count = 1;
+//         allAudio.forEach(track => track.pause());
+//         track.play();
+//         button.innerHTML = `| | &nbsp ${object.name.toUpperCase()}`;
+//     } 
+//     else {
+//         count = 0;
+//         track.pause();
+//         button.innerHTML = `&#9658 &nbsp ${object.name.toUpperCase()}`;
+//     };
+// };
+
+// const buttons = document.querySelectorAll('.mbira, .guitar, .chant, .cretan');
+
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         let data = button.classList.value;
+//         playPause(data, audioData[data]);
+//     });
+// });
+
+// allAudio.forEach(track => {
+//     track.addEventListener('ended', () => {
+//         track.pause();
+
+//     })
+// })
+
+
+///////////////////////////////////////////////////////////////////////
+
+const audioContext = new AudioContext();
+const audioMbira = document.querySelector('.audio__mbira');
+const trackMbira = audioContext.createMediaElementSource(audioMbira);
+trackMbira.connect(audioContext.destination);
+const playButton = document.querySelector('.button__mbira');
+
+
+playButton.addEventListener('click', () => {
+    if (audioContext.state === 'suspended') {
+        audioContext.resume();
     }
-};
-
-
-const musicPlayer = document.querySelector('.music__player');
-const audioDataArray = Object.entries(audioData);
-
-
-audioDataArray.forEach(track => {
-    musicPlayer.insertAdjacentHTML('beforeend', 
-
-    `<div class="player__audio">
-
-        <button class="${track[0]}">&#9658 &nbsp ${track[1].name.toUpperCase()}</button>
-
-        <audio id="${track[0]}" class="audio audio__${track[0]}">
-            <source src="${track[1].url}" type="audio/mp3">
-        </audio>
-    </div>`)
-});
+    if (playButton.dataset.playing === 'false') {
+        audioMbira.play();
+        playButton.dataset.playing = 'true';
+    }
+    else if (playButton.dataset.playing === 'true') {
+        audioMbira.pause();
+        playButton.dataset.playing = 'false';
+    }
+}, false);
 
 
 
-let count = 0;
-
-const playPause = (song, object) => {
-    const allAudio = document.querySelectorAll('#mbira, #guitar, #chant, #cretan');
-    let track = document.querySelector(`#${song}`);
-    let button = document.querySelector(`.${song}`);
-
-    if (count === 0) {
-        count = 1;
-        allAudio.forEach(track => track.pause());
-        track.play();
-        button.innerHTML = `| | &nbsp ${object.name.toUpperCase()}`;
-    } 
-    else {
-        count = 0;
-        track.pause();
-        button.innerHTML = `&#9658 &nbsp ${object.name.toUpperCase()}`;
-    };
-};
-
-const buttons = document.querySelectorAll('.mbira, .guitar, .chant, .cretan');
 
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        let data = button.classList.value;
-        playPause(data, audioData[data]);
-    })
-})
+
+
+
 
